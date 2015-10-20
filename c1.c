c18674817a7aaac631aabe5070ba03fd331181c7
汤威 <18862497279@163.com>
2015-10-20 18:04:43 +0800

    c1.c

--- c1.c
+++ c1.c
@@ -9,9 +9,14 @@ daldjaldjlajdlajdlaa;da;d;akd;ka;dka;kd;akd;akd;ak;kdana;dajdha
 =======
 akdhadajdlajdlajd;jaja
 jdaldjaldjlajdlajdlaa;da;d;akd;ka;dka;kd;akd;akd;ak;kdana;dajdha
+<<<<<<< HEAD
 >>>>>>> parent of 552dad5... 修正c1文件
 11111111
 11111111
+=======
+22222222
+22222222
+>>>>>>> a63c61e... c1.c
 adadad
 a
 d
